f5d473c2111f74898ecb9a4fe22bbb97aeeb65ee
igorriazanovst-debug <igor.riazanov.st@gmail.com>
2026-03-28 21:25:32 +0300

    chore: remove temp fix scripts from repo root

--- fix_webhook.py
+++
@@ -1,7 +0,0 @@
-﻿path = '/app/apps/payments/tests/test_webhook.py'
-with open(path, 'r') as f:
-    lines = f.readlines()
-lines[64] = '    body = b\'{"event": "payment.succeeded"}\'\n'
-with open(path, 'w') as f:
-    f.writelines(lines)
-print('done')
